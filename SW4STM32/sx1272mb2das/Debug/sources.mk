47ec6e055d3b40a793da859b92e0afbdf9d42ac7
FriZIk <tester251100@gmail.com>
2020-06-02 17:16:09 +0300

    02.06.2020

---
+++ SW4STM32/sx1272mb2das/Debug/sources.mk
@@ -0,0 +1,28 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+SIZE_OUTPUT := 
+OBJDUMP_LIST := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+OBJCOPY_BIN := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Drivers/BSP/Components \
+Drivers/BSP/STM32L0xx_Nucleo \
+Drivers/BSP/sx1272mb2das \
+Drivers/CMSIS \
+Drivers/STM32L0xx_HAL_Driver \
+Middlewares/LoRaWAN/Utilities \
+Projects/PingPong \
+Projects/SW4STM32 \
+
---
+++ SW4STM32/sx1276mb1mas/Debug/Middlewares/LoRaWAN/Utilities/queue.d
@@ -0,0 +1,5 @@
+Middlewares/LoRaWAN/Utilities/queue.o: \
+ C:/Users/User/Desktop/en.i-cube_lrwan/STM32CubeExpansion_LRWAN_V1.3.1/Middlewares/Third_Party/LoRaWAN/Utilities/queue.c \
+ C:/Users/User/Desktop/en.i-cube_lrwan/STM32CubeExpansion_LRWAN_V1.3.1/Middlewares/Third_Party/LoRaWAN/Utilities/queue.h
+
+C:/Users/User/Desktop/en.i-cube_lrwan/STM32CubeExpansion_LRWAN_V1.3.1/Middlewares/Third_Party/LoRaWAN/Utilities/queue.h:
